ee927f42be3a37d13125df2513cee2ae99a672ca
Maowss <Maowss@gmail.com>
2019-05-15 00:12:23 -0300

    Estamos desenvolvendo o projeto Carro + Fizemos no DAO o metodo para Inserir, Alterar, Excluir e falta concluir o Selecionar (List) + Qualquer duvida procurar na apostila

---
+++ src/PacoteBD/CarroDao.java
@@ -0,0 +1,86 @@
+package PacoteBD;
+
+import java.sql.*;
+import java.util.*;
+
+public class CarroDao {
+    
+    private Connection con = null;
+
+    public CarroDao(Connection con){
+        setCon(con);
+    }
+    
+    public Connection getCon(){
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+    
+    public String inserir(CarroBean carro){    
+        String sql = "insert into carro(placa, cor, descricao)values(?,?,?)";
+        
+        try{
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ps.setString(1, carro.getPlaca());
+            ps.setString(2, carro.getCor());
+            ps.setString(3, carro.getDescricao());
+            
+            if(ps.executeUpdate()>0){
+                return "Inserido com sucesso."; 
+            }else{
+                return "Erro ao inserir";
+            }
+        }catch (SQLException e){
+            return e.getMessage();
+        }
+    }
+    
+    public String alterar(CarroBean carro){
+        String sql = "update carro set cor = ?,descricao = ?";
+            sql += " where placa = ?";
+            
+            try{
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            
+            ps.setString(1, carro.getCor());
+            ps.setString(2, carro.getDescricao()); 
+            ps.setString(3, carro.getPlaca());
+            
+                if(ps.executeUpdate() > 0){
+                    return "Alterado com sucesso.";
+                }else{
+                    return "Erro ao alterar";
+                }
+            }catch (SQLException e){
+                return e.getMessage();
+            }     
+    }
+    
+    public String exclrir(CarroBean carro){
+        String sql = "delete from carro where placa = ?";
+            
+            try{
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            
+            ps.setString(1, carro.getPlaca());
+            
+                if(ps.executeUpdate() > 0){
+                    return "Excluido com sucesso.";
+                }else{
+                    return "Erro ao excluir";
+                }
+            }catch (SQLException e){
+                return e.getMessage();
+            }     
+    }
+    
+    public List<CarroBean>listarTodos(){
+        String sql = "select * from carro";
+        
+        List<CarroBean> listaCarro = new ArrayList<CarroBean>();
+    }
+   
+}
